26d655db1063fcf094b4d7b2ba0e08a2b29714be
aqingxin <1607440679@qq.com>
2018-11-17 17:14:39 +0800

    歌曲列表组件

---
+++ src/component/ListItem.js
@@ -0,0 +1,54 @@
+import React,{Component} from 'react';
+
+import { Redirect } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+class ListItem extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      source:'',
+      jump:false
+    }
+    this.jumpRouter=this.jumpRouter.bind(this);
+  }
+  jumpRouter(){
+    // this.props.history.push({
+    //   pathname:'/play',
+    //   state:{
+    //     id:3
+    //   }
+    // })
+    // this.props.history.push('play')
+    this.setState({
+      jump:true
+    })
+  }
+  render(){
+    if(this.state.jump){
+      return <Redirect to={{path:'/play',state:{"id":this.props.songInfo.id}}} />;
+    }
+    return (
+      <li className={['newSongItem',this.props.type==='newSong'?'':'hotSongItem'].join(' ')} onClick={this.jumpRouter}>
+      <Link to={'/play/'+this.props.songInfo.id}>
+        <span className="itemIndex">
+          {
+            this.props.type==='hotSong'&&
+            this._reactInternalFiber.key*1+1
+          }
+        </span>
+        <div>{this.props.songInfo.name}</div>
+        <p>
+          <i>SQ</i>
+          {this.props.songInfo.singer}
+        </p>
+        <div className="indexPlayIcon"></div>
+
+      </Link>
+      
+    </li>
+    )
+  }
+}
+
+export default ListItem
